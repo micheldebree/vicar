fe4ee35ced6d9a10846a1ac43a17a3e21e6ca05a
Michel de Bree <michel@micheldebree.nl>
2015-11-04 10:20:06 +0100

    Upgraded to latest bower dependencies. Formatting.

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,8 @@ module.exports = function (grunt) {
         dist: 'dist'
     };
 
+    var serveStatic = require('serve-static');
+
     grunt.loadNpmTasks('grunt-node-webkit-builder');
     
     // Define the configuration for all the tasks
@@ -88,12 +90,12 @@ module.exports = function (grunt) {
                     open: true,
                     middleware: function (connect) {
                     return [
-                      connect.static('.tmp'),
+                      serveStatic('.tmp'),
                       connect().use(
                         '/bower_components',
-                        connect.static('./bower_components')
+                        serveStatic('./bower_components')
                       ),
-                      connect.static(appConfig.app)
+                      serveStatic(appConfig.app)
                     ];
                   }
                 }
@@ -103,13 +105,13 @@ module.exports = function (grunt) {
           port: 9001,
           middleware: function (connect) {
             return [
-              connect.static('.tmp'),
-              connect.static('test'),
+              serveStatic('.tmp'),
+              serveStatic('test'),
               connect().use(
                 '/bower_components',
-                connect.static('./bower_components')
+                serveStatic('./bower_components')
               ),
-              connect.static(appConfig.app)
+              serveStatic(appConfig.app)
             ];
           }
         }
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,96 +1,94 @@
 /*global angular, Image, URL, ImageGrabber, peptoPalette */
 angular.module('vicarApp')
-    .controller('MainCtrl', [
-        '$scope',
-        'c64izerService',
-        '$timeout',
-        function ($scope, c64izerService, $timeout) {
-        'use strict';
+  .controller('MainCtrl', [
+    '$scope',
+    'c64izerService',
+    '$timeout',
+    function ($scope, c64izerService, $timeout) {
+      'use strict';
 
-        var img = new Image();
-        img.src = 'images/eye.jpg';
+      var img = new Image();
+      img.src = 'images/eye.jpg';
 
-        // graphic mode selection
-        $scope.graphicModes = c64izerService.supportedGraphicModes;
-        $scope.selectedGraphicMode = $scope.graphicModes[0];
-        // TODO: add graphics mode selection in future release
-        // $scope.$watch('selectedGraphicMode', function () {
-        //     convert();
-        // });
-        // $scope.selectGraphicMode = function (graphicMode) {
-        //     $scope.selectedGraphicMode = graphicMode;
-        // };
+      // graphic mode selection
+      $scope.graphicModes = c64izerService.supportedGraphicModes;
+      $scope.selectedGraphicMode = $scope.graphicModes[0];
+      // TODO: add graphics mode selection in future release
+      // $scope.$watch('selectedGraphicMode', function () {
+      //     convert();
+      // });
+      // $scope.selectGraphicMode = function (graphicMode) {
+      //     $scope.selectedGraphicMode = graphicMode;
+      // };
 
-        // ordered dithering selection
-        $scope.dithers = c64izerService.getSupportedDithers();
-        $scope.selectedDither = $scope.dithers[0];
+      // ordered dithering selection
+      $scope.dithers = c64izerService.getSupportedDithers();
+      $scope.selectedDither = $scope.dithers[0];
 
-        $scope.selectDither = function (dither) {
-            $scope.selectedDither = dither;
-            convert();
-        };
-
-        // error diffusion dithering selection
-        $scope.errorDiffusionDithers = c64izerService.supportedErrorDiffusionDithers;
-        $scope.selectedErrorDiffusionDither = $scope.errorDiffusionDithers[3];
-
-        $scope.selectErrorDiffusionDither = function (errorDiffusionDither) {
-            $scope.selectedErrorDiffusionDither = errorDiffusionDither;
-            convert();
-        };
-
-        /**
-         * Convert a ColorMap to a PixelImage, for debugging visualisation.
-
-        function toPixelImage(colorMap, palette) {
-            var result = new PixelImage();
-            result.palette = palette;
-            result.dither = [[0]];
-            result.init(colorMap.width, colorMap.height);
-            result.addColorMap(new ColorMap(colorMap.width, colorMap.height, 1, 1));
-            result.drawImageData(colorMap.toImageData(palette));
-            return result;
-        }
-        */
-
-        /**
-         * Convert the current Image to a PixelImage and update the scope.
-         */
-        function convert () {
-
-            var resultImage = $scope.selectedGraphicMode.value(),
-                grabber = new ImageGrabber(img, resultImage.width, resultImage.height);
-
-            $scope.mainImage = undefined;
-
-            resultImage.palette = peptoPalette;
-            resultImage.dither = $scope.selectedDither.value;
-            resultImage.errorDiffusionDither = $scope.selectedErrorDiffusionDither.value;
-
-          
-            grabber.grab(
-              function (imageData) {
-                $scope.$evalAsync(function() {
-                  c64izerService.convertToPixelImage(imageData, resultImage);
-                  $scope.mainImage = resultImage;
-                  $scope.download = resultImage.toDownloadUrl();
-                });
-                
-              }
-            ); 
-            
-
-        }
-        
-        $scope.upload = function(file) {
-            img.src = URL.createObjectURL(file);
-            img.onload = function() { 
-              convert();
-            };
-            
-        };
-        
-        
+      $scope.selectDither = function (dither) {
+        $scope.selectedDither = dither;
         convert();
+      };
 
-    }]);
+      // error diffusion dithering selection
+      $scope.errorDiffusionDithers = c64izerService.supportedErrorDiffusionDithers;
+      $scope.selectedErrorDiffusionDither = $scope.errorDiffusionDithers[3];
+
+      $scope.selectErrorDiffusionDither = function (errorDiffusionDither) {
+        $scope.selectedErrorDiffusionDither = errorDiffusionDither;
+        convert();
+      };
+
+      /**
+       * Convert a ColorMap to a PixelImage, for debugging visualisation.
+
+      function toPixelImage(colorMap, palette) {
+          var result = new PixelImage();
+          result.palette = palette;
+          result.dither = [[0]];
+          result.init(colorMap.width, colorMap.height);
+          result.addColorMap(new ColorMap(colorMap.width, colorMap.height, 1, 1));
+          result.drawImageData(colorMap.toImageData(palette));
+          return result;
+      }
+      */
+
+      /**
+       * Convert the current Image to a PixelImage and update the scope.
+       */
+      function convert() {
+
+        var resultImage = $scope.selectedGraphicMode.value(),
+          grabber = new ImageGrabber(img, resultImage.width, resultImage.height);
+
+        $scope.mainImage = undefined;
+
+        resultImage.palette = peptoPalette;
+        resultImage.dither = $scope.selectedDither.value;
+        resultImage.errorDiffusionDither = $scope.selectedErrorDiffusionDither.value;
+
+        grabber.grab(
+          function (imageData) {
+            $scope.$evalAsync(function () {
+              c64izerService.convertToPixelImage(imageData, resultImage);
+              $scope.mainImage = resultImage;
+              $scope.download = resultImage.toDownloadUrl();
+            });
+
+          }
+        );
+      }
+
+      $scope.upload = function (file) {
+        img.src = URL.createObjectURL(file);
+        img.onload = function () {
+          $scope.$evalAsync(function () {
+            convert();
+          });
+        };
+      };
+
+      convert();
+
+    }
+  ]);
