d2007b6c3f13d3118c3d58020cb10485ee560e75
Michel de Bree <michel@micheldebree.nl>
2016-10-26 08:42:50 +0200

    Stripping everything except the live image.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,28 +1,15 @@
 /*global angular*/
 angular
-      .module('vicarApp', ['ngRoute','ngFileUpload', 'webcam'])
-      .config(function ($routeProvider, $compileProvider) {
+    .module('vicarApp', ['ngRoute', 'webcam'])
+    .config(function ($routeProvider, $compileProvider) {
         'use strict';
-
         $routeProvider
-              .when('/', {
+            .when('/', {
                 templateUrl: 'views/main.html'
-                //controller: 'MainCtrl'
+                    //controller: 'MainCtrl'
             })
-              .when('/koala', {
-                templateUrl: 'views/koala.html',
-                controller: 'KoalaCtrl'
-            })
-              .when('/settings', {
-                templateUrl: 'views/settings.html'
-            })
-              .when('/debug', {
-                templateUrl: 'views/debug.html'
-            })
-              .otherwise({
-                redirectTo: '/'
-            });
-
+        .otherwise({
+            redirectTo: '/'
+        });
         $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|blob|data):/);
-
     });
